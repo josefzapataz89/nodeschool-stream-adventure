e608fdc34094af90fba9802ed135ef255e3a72d3
Jose Fernando Zapata Zambrano <joferzz1989@gmail.com>
2019-11-04 11:27:18 -0400

    transform
    use through2 library to transform the stream

--- transform.js
+++ transform.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const through = require('through2')
+const stream = through(write, end)
+
+function write(buf, encoding, next) {
+    this.push(buf.toString().toUpperCase())
+    next()
+}
+
+function end(done) {
+    done()
+}
+
+process.stdin.pipe(stream).pipe(process.stdout)
